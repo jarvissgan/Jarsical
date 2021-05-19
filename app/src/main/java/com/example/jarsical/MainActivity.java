9afd500f151b84d1ef738c139829003303843261
LegallyAlive <jarvisgan@gmail.com>
2021-05-19 11:34:21 +0800

    Prototype

---
+++ app/src/main/java/com/example/jarsical/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.jarsical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void handleSelection(View myView){
+        String resourceId = getResources().getResourceEntryName(myView.getId());
+        Log.d("Temasek","The id of the pressed ImageButton is: " + resourceId);
+    }
+}
