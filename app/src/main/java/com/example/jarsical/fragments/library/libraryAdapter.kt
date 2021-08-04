651ba7998f988e9e9d3d0973751e99ea711b4809
LegallyAlive <jarvisgan@gmail.com>
2021-08-05 03:08:56 +0800

    Implementing add to library libraryAdapter.kt not working

--- app/src/main/java/com/example/jarsical/fragments/library/libraryAdapter.kt
+++ app/src/main/java/com/example/jarsical/fragments/library/libraryAdapter.kt
@@ -1,40 +1,54 @@
 package com.example.jarsical.fragments.library
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.example.jarsical.R.layout.custom_row
-import com.example.jarsical.SongCollection
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.jarsical.R
+import com.example.jarsical.R.layout.custom_row2
+import com.example.jarsical.Song
+import com.example.jarsical.fragments.library.libraryAdapter.MyViewHolder
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.custom_row.view.*
 
-class libraryAdapter: RecyclerView.Adapter<libraryAdapter.MyViewHolder>() {
+class libraryAdapter(favList: ArrayList<Song>): Adapter<MyViewHolder>()  {
 
-    var songCollection = SongCollection()
+    var favList: ArrayList<Song> = ArrayList()
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
+    class MyViewHolder(itemView: View): ViewHolder(itemView){
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(custom_row, parent, false))
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(custom_row2, parent, false))
+
     }
 
     override fun getItemCount(): Int {
-        return songCollection.songs.size
+        return favList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        /*val currentItem = songCollection.songs[position]
+        var currentItem = favList[position]
 
         holder.itemView.txtSongName.text = currentItem.title
         holder.itemView.txtSongArtist.text = currentItem.artists
         holder.itemView.txtSongLength.text = currentItem.songLength.toString()
+
         //picasso allows us to use cover art without downloading
         Picasso.get().load(currentItem.artLink).placeholder(R.drawable.ic_baseline_error_24).into(holder.itemView.imageButton)// loads images into imageButton
         //sets content description
         holder.itemView.imageButton.contentDescription = currentItem.id
         val test = currentItem.id
-        Log.d("onBind",test)*/
+        Log.d("libraryAdapter",test)
+
+    }
+
+    fun setData(song: ArrayList<Song>){
+        this.favList = song
+        notifyDataSetChanged()
+        Log.d("setData",favList.size.toString())
 
     }
 }
--- app/src/main/java/com/example/jarsical/fragments/list/songAdapter.kt
+++ app/src/main/java/com/example/jarsical/fragments/list/songAdapter.kt
@@ -1,12 +1,12 @@
 package com.example.jarsical.fragments.list
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jarsical.R
-import com.example.jarsical.R.layout.custom_row
+import com.example.jarsical.R.layout.custom_row2
+import com.example.jarsical.Song
 import com.example.jarsical.SongCollection
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.custom_row.view.*
@@ -20,7 +20,7 @@ class songAdapter: RecyclerView.Adapter<songAdapter.MyViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(custom_row, parent, false))
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(custom_row2, parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -36,9 +36,17 @@ class songAdapter: RecyclerView.Adapter<songAdapter.MyViewHolder>() {
         //picasso allows us to use cover art without downloading
         Picasso.get().load(currentItem.artLink).placeholder(R.drawable.ic_baseline_error_24).into(holder.itemView.imageButton)// loads images into imageButton
         //sets content description
-        holder.itemView.imageButton.contentDescription = currentItem.id
-        val test = currentItem.id
-        Log.d("onBind",test)
+        holder.itemView.buttonTest.contentDescription = currentItem.id
 
+        /*Log.d("test", holder.itemView.buttonTest.contentDescription.toString())*/
+
+
+        val test = currentItem.id
+        /*Log.d("onBind",test)*/
+
+    }
+    fun setData(song: Array<Song>){
+        this.songCollection.songs = song
+        notifyDataSetChanged()
     }
 }
